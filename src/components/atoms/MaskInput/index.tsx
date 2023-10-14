e0183c26fe739683c4cc4bb51286e1f03e0ee9a9
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2023-10-14 11:44:44 -0300

    first render

--- src/components/atoms/MaskInput/index.tsx
+++ src/components/atoms/MaskInput/index.tsx
@@ -5,7 +5,7 @@ import {TextInputMask} from 'react-native-masked-text';
 const MaskInput = ({type, value, onChangeText}: any) => {
   return (
     <TextInputMask
-      style={{width: '100%', height: 52}}
+      style={{width: '100%', height: 52, color: '#5555'}}
       type={type}
       value={value}
       onChangeText={text => {
