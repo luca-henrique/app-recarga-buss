29559a267b4dc6ef5b3de42cd02a3474e039bd2d
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2023-11-11 11:39:17 -0300

    config eslint airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,21 @@
 module.exports = {
   root: true,
+  root: true,
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
 
-  extends: ['@react-native', 'prettier'],
+  extends: [
+    '@react-native',
+    'airbnb-typescript/base',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
 
   rules: {
     'prettier/prettier': 'error',
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Task
 
-   - [] - Eslint & Prettier
+- [x] - Eslint & Prettier
+- [] - Criar teste simples com jest
+- [] - Gerar coverage do projeto
+- [] - Configurar husk para commit
+- [] - Absolut imports
+- [] - Alterar nome do projeto
